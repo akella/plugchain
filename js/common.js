880d908f6006462464ee88d4dc57bbf9799b2349
akella <akella.a@gmail.com>
2014-10-14 17:28:42 +0300

    gogogo

--- js/common.js
+++ js/common.js
@@ -1,8 +1,55 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$('.navigation__plus').on('click', function() {
+		$(this).parents('body').find('.sidemenu').toggleClass('is-open');
+		$(this).toggleClass('is-x');
+		return false;
+	});
+	//SELECTS
+	$(document).ready(function() {
+	    $(document).click(function() {
+	        $(".js-select-list").hide();
+	        $(".js-select").removeClass("is-active");
+	    });
+	    function selectList() {
+	        var select = $(".js-select");
+	        var select_list = $(".js-select-list");
+	        $("body").on("click", ".js-select", function(event){
+	            if ($(this).hasClass("is-active")) {
+	                select.removeClass("is-active");
+	                select_list.hide();
+	            }
+	            else {
+	                select.removeClass("is-active");
+	                select_list.hide();
+	                $(this).find(".js-select-list").show();
+	                $(this).addClass("is-active");
+	            }
+	            event.stopPropagation();
+	        });
+	        $("body").on("click", ".js-select-list li", function(event){
+	            var id = $(this).attr("data-id");
+	            var text = $(this).text();
+	            $(this).parents(".js-select").find(".js-select-text").text(text);
+	            $(this).parents(".js-select").find(".js-select-input").val(id);
+	            $(this).parent().hide();
+	            $(this).parents(".js-select").removeClass("is-active");
+	            event.stopPropagation();
+	        });
+	    }  
+	    selectList();
+	    $("body").on("click", ".js-select", function(event){
+	        event.stopPropagation();
+	    });
+	});
+	//api swith
+	$('.switch__buttons').on('click',function() {
+		$(this).parent('.switch').find('.switch__buttons').toggleClass('is-active');
+		return false;
+	});
 
-	console.log($('body').html());
+
+	//scroll
+	$('#pane').jScrollPane({
+	});
 });
--- js/init.js
+++ js/init.js
@@ -1,3 +1,6 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/tabs.js",
+					"js/lib/jScrollPane.min.js",
+					"js/lib/jquery.mousewheel.js",
 					"js/common.js");
